5434358a04908d23f83977d6535cd6ba7464c281
justix <justix36@gmail.com>
2022-07-27 17:46:56 +0300

    Updates

---
+++ precache-manifest.ce492a83496770d7cd8f9c507989c830.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e92ca2bff260c83f23f",
+    "url": "/react-shop/static/css/main.2c98fdcd.chunk.css"
+  },
+  {
+    "revision": "5e92ca2bff260c83f23f",
+    "url": "/react-shop/static/js/main.5e92ca2b.chunk.js"
+  },
+  {
+    "revision": "dd688a8cdf5beaac8aeb",
+    "url": "/react-shop/static/js/1.dd688a8c.chunk.js"
+  },
+  {
+    "revision": "4b3be4f6cafc0ba0a031",
+    "url": "/react-shop/static/js/runtime~main.4b3be4f6.js"
+  },
+  {
+    "revision": "5afc68c918f7f5c1d6ba45ee6ad48da1",
+    "url": "/react-shop/index.html"
+  }
+];
